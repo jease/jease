4ec573c9f0d979ebf70a33f9f5aa01c409d74e50
ghaseminya <ghaseminya@gmail.com>
2018-06-04 18:04:20 +0430

    remove some unnedded PersistenceEnginePerst function

--- src/main/java/jfix/db4o/engine/PersistenceEnginePerst.java
+++ src/main/java/jfix/db4o/engine/PersistenceEnginePerst.java
@@ -16,166 +16,98 @@
  */
 package jfix.db4o.engine;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.Writer;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 import org.garret.perst.Storage;
 import org.garret.perst.StorageFactory;
-import org.garret.perst.XMLImportException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class PersistenceEnginePerst extends PersistenceEngineBase implements PersistenceEngine {
+public class PersistenceEnginePerst implements PersistenceEngine {
 
+    protected String directory;
+    protected String filename;
     protected Storage db;
     protected Set<Object> root;
 
-    private final Logger logger = LoggerFactory.getLogger(PersistenceEnginePerst.class);
-
-    @Override
-    protected String getEngineName() {
-        return "perst";
-    }
-
-    @Override
-    protected String getEngineFileName() {
-        return "perst.odb";
-    }
-
-    @Override
     public void open(String database) {
         initDirectory(database);
         openEngine();
     }
 
-    private void createDb() {
-        db = StorageFactory.getInstance().createStorage();
-        db.setProperty("perst.file.noflush", Boolean.FALSE); // from time to time perst db is getting corrupted for no obvious reason
-        db.setProperty("perst.object.cache.kind", "strong");
+    protected void initDirectory(String database) {
+        if (database.contains(File.separator)) {
+            directory = database.endsWith(File.separator) ? database : database + File.separator;
+        } else {
+            directory = System.getProperty("user.home") + File.separator + "perst" + File.separator + database
+                    + File.separator;
+        }
+        filename = directory + "perst.odb";
+        new File(directory).mkdirs();
     }
 
-    @SuppressWarnings("unchecked")
     protected void openEngine() {
-        createDb();
+        db = StorageFactory.getInstance().createStorage();
+        db.setProperty("perst.file.noflush", Boolean.TRUE);
+        db.setProperty("perst.object.cache.kind", "strong");
         db.open(filename);
         if (db.getRoot() == null) {
             root = db.createSet();
             db.setRoot(root);
         } else {
-            if (!runGc()) { // let's try to fix/clean up database before start working
-                runRecovery();
-            }
             root = (Set<Object>) db.getRoot();
         }
     }
 
-    private boolean runRecovery() {
-        logger.info("Recovery started");
-        try {
-            Writer writer = new BufferedWriter(new FileWriter(filename + ".recovery"));
-            // export the whole database to the specified writer in XML format
-            db.exportXML(writer);
-            writer.close(); // exportXML doesn't close the stream, close it here
-        } catch (IOException e) {
-            logger.error("Export failed: " + e);
-            return false;
-        }
-        root = db.createSet();
-        db.setRoot(root);
-        try {
-            Reader reader = new BufferedReader(new FileReader(filename + ".recovery"));
-            db.importXML(reader);
-            reader.close(); // importXML doesn't close the stream, close it here
-        } catch (IOException | XMLImportException e) {
-            logger.error("Import failed: " + e);
-            return false;
-        }
-        logger.info("Recovery finished");
-        return true;
+    public String getBlobDirectory() {
+        return directory;
     }
 
-    @Override
     public Collection<Object> query() {
-        Set<Object> rslt = new HashSet<>(root.size());
-        Iterator<Object> iter = root.iterator();
-        int added = 0;
-        while (iter.hasNext()) {
-            try {
-                Object obj = iter.next();
-                rslt.add(obj);
-                added++;
-            } catch (Exception e) {
-                logger.error("Root size: {}; Added: {}; {}", root.size(), added, e);
-                throw e; // we cannot continue, database is broken
-            }
-        }
-        return rslt;
+        return new HashSet<>(root);
     }
 
-    @Override
     public void save(Object object) {
         root.add(object);
         db.modify(object);
     }
 
-    @Override
     public void delete(Object object) {
         root.remove(object);
         db.deallocate(object);
-        runGc();
+        db.gc();
     }
 
-    private boolean runGc() {
-        try {
-            db.gc();
-            return true;
-        } catch (Exception e) {
-            logger.error("{}", e);
-            return false;
-            // perhaps we can safely continue
-        }
-    }
-
-    @Override
     public void begin() {
         // Empty as Perst don't needs an explicit transaction begin.
     }
 
-    @Override
     public void commit() {
         db.commit();
     }
 
-    @Override
     public void rollback() {
         db.rollback();
     }
 
-    @Override
     public void backup() {
         try {
-            String backupFilename = getBackupFileName();
+            String backupFilename = filename + new SimpleDateFormat("-yyyyMMdd").format(new Date());
             db.backup(new FileOutputStream(new File(backupFilename)));
-            logger.info("Backup successful: " + backupFilename);
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage(), e);
         }
     }
 
-    @Override
     public void close() {
         db.close();
     }
 
+    public String toString() {
+        return filename;
+    }
 }
